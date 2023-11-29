60382dcc62cfa1f323ccd8fecf01075be488aa43
NWoodsman <14882714+NWoodsman@users.noreply.github.com>
2023-11-28 20:10:50 -0700

    Add benchmarks
    Added some benchmarks of ImmutableList.Builder to compare to dot net's immutable list.

--- ImmutableCollections/test/Benchmarks/BenchmarkVectors.cs
+++ ImmutableCollections/test/Benchmarks/BenchmarkVectors.cs
@@ -13,6 +13,7 @@ namespace Benchmarks
     public class BenchmarkVectors
     {
         const int SIZE = 1_000_000;
+        const int HALF_SIZE = 500_000;
 
         [Benchmark]
         public void ms_mutable_list()
@@ -22,10 +23,50 @@ namespace Benchmarks
         }
 
         [Benchmark]
+        public void ms_builder_good()
+        {
+            var ms_list = ImmutableList<int>.Empty;
+            var builder = ms_list.ToBuilder();
+
+            for (int i = 0; i < SIZE; i++)builder.Add(i);
+            
+            ms_list = builder.ToImmutableList();
+        }
+
+        [Benchmark]
+        public void ms_builder_bad()
+        {
+            var ms_list = ImmutableList<int>.Empty;
+            for (int i = 0; i < SIZE; i++) 
+            {
+                var builder = ms_list.ToBuilder();
+                builder.Add(i);
+                ms_list = builder.ToImmutableList(); 
+            }
+        }
+
+        [Benchmark]
+        public void ms_builder_unpack()
+        {
+            var ms_list = ImmutableList<int>.Empty;
+            var builder = ms_list.ToBuilder();
+
+            for (int i = 0; i < HALF_SIZE; i++) builder.Add(i);
+
+            ms_list = builder.ToImmutableList();
+
+            builder = ms_list.ToBuilder();
+
+            for (int i = HALF_SIZE; i < SIZE; i++) builder.Add(i);
+
+            ms_list = builder.ToImmutableList();
+        }
+        
+        [Benchmark]
         public void ms_immutable_list()
         {
             var ms_list = ImmutableList<int>.Empty;
-            for(int i=0;i<SIZE;i++) ms_list = ms_list.Add(i);  
+            for (int i = 0; i < SIZE; i++) ms_list = ms_list.Add(i);
         }
 
         [Benchmark]
